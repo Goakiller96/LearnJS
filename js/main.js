75bbb1040856a98cb4ea42902ab524a462b9dd60
Andrey <kiseleffav@gmail.com>
2021-04-21 19:51:43 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// $(function(){
+
+// });
+
+'use strict';
+
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+const category = 'toys';
+alert(`Вы выбрали раздел - ${category}`);
