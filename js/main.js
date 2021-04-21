0789f835f4815fbe6445d1580d6e4a03a11a0f18
Andrey <kiseleffav@gmail.com>
2021-04-21 20:47:12 +0500

    Lesson-12

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-// $(function(){
-
-// });
-
 'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
-// const category = 'toys';
-// console.log(`https://someurl.com/${category}/5`);
+const personalMovieDB = {
+      count: numberOfFilms,
+      movies: {},
+      actors: {},
+      genres: [],
+      privat: false
+};
 
-const category = 'toys';
-alert(`Вы выбрали раздел - ${category}`);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
